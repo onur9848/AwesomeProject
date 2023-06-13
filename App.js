5d120b966b2018d33f84eecac61003a26ee41c7d
onur9848 <1030516703@erciyes.edu.tr>
2023-06-13 12:27:28 +0300

    a

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>OMerhabas deneme 2 çalış d!</Text>
+      <TextInput style={{height:40, borderColor:'gray', borderWidth:1,width:100}}/>
+      
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f2da21',
     alignItems: 'center',
     justifyContent: 'center',
   },
